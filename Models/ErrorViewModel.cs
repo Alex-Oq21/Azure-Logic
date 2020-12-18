0ac7fa8be184d543718a4a2198a39fc90b16af76
Alexis <omaroquendo221@gmail.com>
2020-12-18 16:51:42 -0500

    Commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LogicWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
